ca3edf8bac7d83b1a9bc1360a63bdc37508c8db4
Ken DeLand <kdeland@axon.com>
2018-08-24 11:08:09 -0700

    Presentation

---
+++ theme.js
@@ -0,0 +1,9 @@
+import { condensed } from 'mdx-deck/themes'
+import prismStyle from 'react-syntax-highlighter/styles/prism/tomorrow'
+
+export default {
+  ...condensed,
+  prism: {
+    style: prismStyle
+  }
+}
